27825f02275e0785df96e63badf8280b21f97f58
prithaxx <105132736+prithaxx@users.noreply.github.com>
2025-01-10 00:48:05 -0500

    text generation updated

--- ANPR/main.py
+++ ANPR/main.py
@@ -44,3 +44,4 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 res = cv2.putText(img, text=text, org=(approx[0][0][0], approx[1][0][1]+60), fontFace=font, fontScale=1, color=(0,255,0), thickness=2, lineType=cv2.LINE_AA)
 res = cv2.rectangle(img, tuple(approx[0][0]), tuple(approx[2][0]), (0,255,0),3)
 plt.imshow(cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+plt.show()
